3bb0cb4b833fc6ec3a35f02ec2900296a174897c
get4137 <get4137@gmail.com>
2019-07-03 17:21:38 +0300

    fix cancel button, published form

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ChecklistsController < ApplicationController
-  load_and_authorize_resource
+  before_action :authenticate_user!
   def new
     # binding.pry
     @form = Form.find(params[:checklist][:form_id])
