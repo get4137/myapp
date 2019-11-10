a480f1ed31429a4d709592536108760669af664d
get4137 <get4137@gmail.com>
2019-11-10 20:58:33 +0200

    upgrade ruby to 2.6.5

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.3'
+ruby '2.6.5'
 
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'cancancan'
@@ -14,28 +14,27 @@ gem 'haml'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'materialize-sass'
-gem 'pg', '>= 0.18', '< 2.0'
+gem 'pg'
 gem 'puma', '~> 3.11'
 gem 'rails', '~> 5.2.3'
 gem 'rolify'
-gem 'sass-rails', '~> 5.0'
-gem 'turbolinks', '~> 5'
-gem 'uglifier', '>= 1.3.0'
+gem 'sass-rails'
+gem 'turbolinks'
+gem 'uglifier'
 
 group :development, :test do
-  gem 'pry-nav'
-  gem 'pry-rails'
+  gem 'pry'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
-  gem 'listen', '>= 3.0.5', '< 3.2'
-  gem 'web-console', '>= 3.3.0'
+  gem 'listen'
+  gem 'web-console'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'spring-watcher-listen'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,82 +10,83 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_703_120_727) do
+ActiveRecord::Schema.define(version: 2019_07_03_120727) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.string 'value'
-    t.text 'comment'
-    t.bigint 'checklist_id'
-    t.bigint 'question_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['checklist_id'], name: 'index_answers_on_checklist_id'
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+  create_table "answers", force: :cascade do |t|
+    t.string "value"
+    t.text "comment"
+    t.bigint "checklist_id"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["checklist_id"], name: "index_answers_on_checklist_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'checklists', force: :cascade do |t|
-    t.string 'project_uid'
-    t.bigint 'form_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['form_id'], name: 'index_checklists_on_form_id'
-    t.index ['user_id'], name: 'index_checklists_on_user_id'
+  create_table "checklists", force: :cascade do |t|
+    t.string "project_uid"
+    t.bigint "form_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["form_id"], name: "index_checklists_on_form_id"
+    t.index ["user_id"], name: "index_checklists_on_user_id"
   end
 
-  create_table 'forms', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.string 'status', default: 'draft'
-    t.datetime 'published_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "forms", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "status", default: "draft"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.bigint 'form_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['form_id'], name: 'index_questions_on_form_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "form_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["form_id"], name: "index_questions_on_form_id"
   end
 
-  create_table 'roles', force: :cascade do |t|
-    t.string 'name'
-    t.string 'resource_type'
-    t.bigint 'resource_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
-    t.index %w[resource_type resource_id], name: 'index_roles_on_resource_type_and_resource_id'
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table 'users_roles', id: false, force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'role_id'
-    t.index ['role_id'], name: 'index_users_roles_on_role_id'
-    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
-    t.index ['user_id'], name: 'index_users_roles_on_user_id'
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
-  add_foreign_key 'answers', 'checklists'
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'checklists', 'forms'
-  add_foreign_key 'checklists', 'users'
-  add_foreign_key 'questions', 'forms'
+  add_foreign_key "answers", "checklists"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "checklists", "forms"
+  add_foreign_key "checklists", "users"
+  add_foreign_key "questions", "forms"
 end
